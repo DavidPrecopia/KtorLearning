a0223f614481ad150d32b9fb969878cb43b43bc0
DavidPrecopia <35779726+DavidPrecopia@users.noreply.github.com>
2022-02-13 15:13:59 -0500

    Added Ktor dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,12 +3,19 @@ plugins {
 }
 
 group = "com.learning"
-version = "0.1.0"
+version = "0.0.1"
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
+    //importing Kotlin
     implementation(kotlin("stdlib"))
+    //core ktor
+    implementation("io.ktor:ktor-server-core:1.6.7")
+    //engine
+    implementation("io.ktor:ktor-server-netty:1.6.7")
+    //logging - based upon Log4j
+    implementation("ch.qos.logback:logback-classic:1.2.10")
 }
