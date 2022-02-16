09f32199cb0cee1f4370466fd164a118ae8f9de7
DavidPrecopia <35779726+DavidPrecopia@users.noreply.github.com>
2022-02-15 21:14:24 -0500

    Added application.conf and logback.xml
    And made the appropriate changes to Application.kt. Moving from an embedded server to EngineMain.
    
    application.conf, to the best of my knowledge now, is the config file for the whole project. Possibly comparable to the Manifest in Android.

--- src/main/kotlin/com/learning/Application.kt
+++ src/main/kotlin/com/learning/Application.kt
@@ -1,20 +1,24 @@
 package com.learning
 
 import io.ktor.application.*
-import io.ktor.response.*
-import io.ktor.routing.*
-import io.ktor.server.engine.*
-import io.ktor.server.netty.*
 
-fun main() {
-    embeddedServer(Netty, port = 8080) {
-        routing {
-            get("/") {
-                call.respondText("Hello, world!")
-            }
-            get("/norsk") {
-                call.respondText("Hallo og god morgen!")
-            }
-        }
-    }.start(wait = true)
+fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
+
+fun Application.module() {
+
 }
+
+
+// Simpler embedded server - now using application.conf, so need EngineMain
+//fun main() {
+//    embeddedServer(Netty, port = 8080) {
+//        routing {
+//            get("/") {
+//                call.respondText("Hello, world!")
+//            }
+//            get("/norsk") {
+//                call.respondText("Hallo og god morgen!")
+//            }
+//        }
+//    }.start(wait = true)
+//}
